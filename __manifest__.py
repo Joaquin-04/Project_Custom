2943ffb0904519f0f28c01ee13a10aa2a374dcb8
joaquin <joaquin.tejerina@outsourcearg.com>
2025-01-22 11:55:55 -0300

    <fix> Proyecto | sacando los xml vacios

--- __manifest__.py
+++ __manifest__.py
@@ -6,11 +6,7 @@
     'depends': ['project', 'sale', 'purchase', 'stock'],
     'data': [
         'data/ir_sequence_data.xml',
-        'views/project_views.xml',
         'views/res_config_views.xml',
-        'views/sale_order_views.xml',
-        'views/purchase_order_views.xml',
-        'views/stock_picking_views.xml',
         'security/ir.model.access.csv',
     ],
     'installable': True,
