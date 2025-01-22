d155b92c39e1ddf1ec6029399a15278952ac6f75
joaquin <joaquin.tejerina@outsourcearg.com>
2025-01-22 12:09:33 -0300

    <fix> Proyecto | sacando el xmls de res_config_views.xml del manifest

--- __manifest__.py
+++ __manifest__.py
@@ -6,7 +6,6 @@
     'depends': ['project', 'sale', 'purchase', 'stock'],
     'data': [
         'data/ir_sequence_data.xml',
-        'views/res_config_views.xml',
         'security/ir.model.access.csv',
     ],
     'installable': True,
